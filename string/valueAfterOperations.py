f1cfafbec4e0878abd6e5de2491c07d895db74ff
Hamad Ahmad <82972172+Hamad-98@users.noreply.github.com>
2022-03-13 19:07:19 -0400

    Delete valueAfterOperations.py

--- string/valueAfterOperations.py
+++
@@ -1,8 +0,0 @@
-def finalValueAfterOperations(operations):
-  final = 0
-  for word in operations:
-      if word == '--X' or word == 'X--':
-          final -= 1
-      elif word == 'X++' or word == '++X':
-          final += 1
-  return final
